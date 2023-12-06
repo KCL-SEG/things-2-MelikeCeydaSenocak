f59ef94adbdb92d5bfd1b208da670379b717f681
Melike Ceyda Senocak <melike_ceyda.senocak@kcl.ac.uk>
2023-12-06 20:52:41 +0000

    First commit.

--- things/forms.py
+++ things/forms.py
@@ -4,11 +4,9 @@ from .models import Thing
 class ThingForm(forms.ModelForm):
     class Meta:
         model = Thing
-        fields = ['name', 'description', 'quantity']
-        exclude = ['created_at']  # To exclude 'created_at' field from the form
+        fields = ['name', 'description', 'quantity'] 
 
-    # You can customize field widgets here if needed
     widgets = {
-        'description': forms.Textarea(attrs={'rows': 4, 'cols': 40}),  # Display description as Textarea
-        'quantity': forms.NumberInput(attrs={'min': 0, 'max': 50}),  # Display quantity as NumberInput
+        'description': forms.Textarea(attrs={'rows': 4, 'cols': 40, 'maxlength': 120}),  
+        'quantity': forms.NumberInput(attrs={'min': 0, 'max': 50}),  
     }
