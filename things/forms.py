fdba7d41071b0a08ba88fd377bd0e44fea93d65e
Melike Ceyda Senocak <melike_ceyda.senocak@kcl.ac.uk>
2023-12-06 20:55:24 +0000

    Third commit.

--- things/forms.py
+++ things/forms.py
@@ -6,7 +6,7 @@ class ThingForm(forms.ModelForm):
         model = Thing
         fields = ['name', 'description', 'quantity'] 
 
-    widgets = {
-        'description': forms.Textarea(attrs={'rows': 4, 'cols': 40, 'maxlength': 120}),  
-        'quantity': forms.NumberInput(attrs={'min': 0, 'max': 50}),  
-    }
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 4, 'cols': 40, 'maxlength': 120}),  
+            'quantity': forms.NumberInput(attrs={'min': 0, 'max': 50}),  
+        }
