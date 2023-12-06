0b6790c38c6a3b3b1699298c1672a3618865e241
Melike Ceyda Senocak <melike_ceyda.senocak@kcl.ac.uk>
2023-12-06 20:48:16 +0000

    First commit.

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,14 @@
-"""Forms of the project."""
+from django import forms
+from .models import Thing
 
-# Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        exclude = ['created_at']  # To exclude 'created_at' field from the form
+
+    # You can customize field widgets here if needed
+    widgets = {
+        'description': forms.Textarea(attrs={'rows': 4, 'cols': 40}),  # Display description as Textarea
+        'quantity': forms.NumberInput(attrs={'min': 0, 'max': 50}),  # Display quantity as NumberInput
+    }
